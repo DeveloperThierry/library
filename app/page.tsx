729693d360e5e779e3c78ca9264d7a27d18e6cf1
developerthierry <developerthierry@gmail.com>
2025-12-30 02:19:18 +0000

    Init commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Hola</Button>
+    </div>
+  )
+}
+
+export default Home
