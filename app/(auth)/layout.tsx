68819d2e75a06548b70c76f5f7823ab2717e95fc
developerthierry <developerthierry@gmail.com>
2026-01-12 17:43:13 +0000

    Create Route Groups

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react'
+
+const layout = ({children}:{children:ReactNode}) => {
+  return (
+    <main className="auth-container">{children}</main>
+  )
+}
+
+export default layout
